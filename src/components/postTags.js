78aa41e24efca4944abf3ff5dadb7caf67338791
Davide0097 <davidefxfx@gmail.com>
2023-04-27 18:47:52 +0200

    blog post cards

--- src/components/postTags.js
+++ src/components/postTags.js
@@ -17,7 +17,7 @@ const postTags = ({ post }) => {
   const content = post.postHeader.split(" ");
 
   return (
-    <div className="grid grid-cols-4 my-2">
+    <div className="grid grid-cols-2 sm:grid-cols-4 my-2">
       {content.map((substring) => {
         let element;
         if (substring === "tailwind") {
